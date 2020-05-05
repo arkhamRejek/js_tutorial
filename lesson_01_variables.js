81959ac95762db090afe798c23a58ccb4e4f4ab8
Paul Brunache <paulbrunache@gmail.com>
2020-05-05 16:46:15 -0400

    lesson 1 code

---
+++ lesson_01_variables.js
@@ -0,0 +1,9 @@
+// variables
+
+let x = 5.5 // this is a float (just a number with a decimal value)
+let y = 2 // this is an integer ( just a number)
+let z = 'hello world' // this is a string (just words wrapped in quotes)
+let bo = true // this is a boolean ( true or false)
+
+console.log(x + y)
+console.log(z)
